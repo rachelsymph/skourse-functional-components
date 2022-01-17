80141add8fec9c6078e5af8ea9d1bbeed4042ee2
Rachel <rachel@symph.co>
2022-01-17 20:27:51 +0800

    Uses functional components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import './App.css';
 import AddPostButton from './AddPostButton';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [
-        {name: "Gian", content:"Gwapa kay ka", likes:2},
-        {name: "Shad", content:"Pogi ko", likes:0},
-        {name: "Carlos", content:"Hanging around", likes:0},
-        {name: "Reyner", content:"Pagod na ako!", likes:0},
-      ]
-    }
+function App (){
+
+  const [posts, setPosts] = useState([
+    {name: "Gian", content:"Gwapa kay ka", likes:2},
+    {name: "Shad", content:"Pogi ko", likes:0},
+    {name: "Carlos", content:"Hanging around", likes:0},
+    {name: "Reyner", content:"Pagod na ako!", likes:0},
+  ])
+
+  const addPost = () => {
+    setPosts([...posts, {name: "Gian", content:"Gwapa kay ka", likes:2}])
+    
   }
 
-  addPost = () => {
-    this.setState({
-       posts: [...this.state.posts, {name: "Gian", content:"Gwapa kay ka", likes:2}]
-    })
-  }
-
-
-  render() {
-    return (
-      <div className='app'>
-        {this.state.posts.map(post => (
-          <Post name={post.name} content={post.content} likes={post.likes} />
-        ))}
-        <AddPostButton addPostHandler={this.addPost} />
-      </div>
-    );
-  }}
-
+  return (
+    <div className='app'>
+      {posts.map(post => (
+        <Post name={post.name} content={post.content} likes={post.likes} />
+      ))}
+      <AddPostButton addPostHandler={addPost} />
+    </div>
+  );
+}
 
 export default App;
